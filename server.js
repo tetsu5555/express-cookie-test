0865f5a453450c17b04b4f1ac56026af5b4afacb
tetsu5555 <tetete1118@gmail.com>
2019-07-20 05:08:19 +0900

    feat: add some endpoints

--- server.js
+++ server.js
@@ -4,13 +4,16 @@
 var cons = require('./myconstants.js');
 const users = require("./jsons/users.json");
 const shops = require("./jsons/shops.json");
+const spaces = require("./jsons/spaces.json");
 
 // 必要なパッケージの読み込み
 var express = require('express');
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
+const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 
@@ -55,10 +58,45 @@ app.get('/v1/spaces', function (req, res) {
     res.json(cons.spaces)
 })
 
+app.get('/v1/spaces/:id', function (req, res) {
+    const id = req.params.id;
+    const space = spaces.filter(space => {
+        return space.id == id;
+    });
+    res.json(space[0]);
+})
+
+app.get('/v1/shops/:id/spaces', (req, res) => {
+    const id = req.params.id;
+    const res_spaces = spaces.filter(space => {
+        return space.shop_id == id;
+    });
+    res.json(res_spaces);
+})
+
 app.get('/v1/shops', function (req, res) {
     res.json(shops)
 })
 
+app.get('/v1/shops/:id', (req, res) => {
+    const id = req.params.id;
+    const shop = shops.filter(shop => {
+        return shop.id == id;
+    });
+    res.json(shop[0]);
+})
+
+app.get('/v1/users', function (req, res) {
+    res.json(users)
+})
+
+app.get('/v1/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.filter(user => {
+        return user.id == id;
+    });
+    res.json(user);
+})
 
 
 
